f4202682766938b5b66785ab70f621426cb2ea35
Jackson Hsieh <jackson97042@gmail.com>
2020-12-15 15:44:39 +0800

    refactor index and show routes and pages

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = 3000;
 //import handlebars template engine
 const exphbs = require('express-handlebars');
 
-const restaurantList = require('./restaurant.json');
+// const restaurantList = require('./restaurant.json');
+//import Restaurant model
+const Restaurant = require('./models/restaurant')
 
 //import mongoose
 const mongoose = require('mongoose')
@@ -30,31 +32,36 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results })
+    Restaurant.find()
+            .lean()
+            .then(restaurants => res.render('index', { restaurants }))
+            .catch(error => console.log(error))
+    
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
     console.log(req.params.restaurant_id);
     const id = req.params.restaurant_id;
-    const restaurant_info = restaurantList.results.find(restaurant => restaurant.id.toString() === id);
-    
-    res.render('show', { restaurant:  restaurant_info });
+    return Restaurant.findById(id) 
+                .lean()
+                .then(restaurant => res.render('show', { restaurant }))
+                .catch(error => console.log(error))
 })
 
-app.get('/search', (req, res, next) => {
-    console.log(req.query);
-    const keyword = req.query.keyword.trim();
-    if (keyword === "") return res.redirect('/') //without return, it will continue execution, and cause cant set header error
-    const filteredRestaurants = restaurantList.results.filter(restaurant => 
-         restaurant.name.toLowerCase().includes(keyword.toLowerCase()) 
-        || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-    );
-    if (filteredRestaurants.length === 0) {
-        res.render('none', { keyword: keyword })
-    } else {
-        res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
-    }
-})
+// app.get('/search', (req, res, next) => {
+//     console.log(req.query);
+//     const keyword = req.query.keyword.trim();
+//     if (keyword === "") return res.redirect('/') //without return, it will continue execution, and cause cant set header error
+//     const filteredRestaurants = restaurantList.results.filter(restaurant => 
+//          restaurant.name.toLowerCase().includes(keyword.toLowerCase()) 
+//         || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+//     );
+//     if (filteredRestaurants.length === 0) {
+//         res.render('none', { keyword: keyword })
+//     } else {
+//         res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
+//     }
+// })
 
 
 //start server listening
